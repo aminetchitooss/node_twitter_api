4793352ae5a030e8b4a726ac1844a38177587c88
amine.tchitooss <amine.tchitooss@gmail.com>
2020-05-18 20:26:22 +0200

    twit Api

--- config.js
+++ config.js
@@ -1,10 +1,37 @@
 require('dotenv').config();
+const CryptoJS = require("crypto-js");
 
-module.exports = {
-    consumer_key: process.env.CONSUMER_KEY,
-    consumer_secret: process.env.CONSUMER_SECRET,
-    access_token: process.env.ACCESS_TOKEN,
-    access_token_secret: process.env.ACCESS_TOKEN_SECRET,
-    // timeout_ms: 60 * 1000,  // optional HTTP request timeout to apply to all requests.
-    // strictSSL: true,     // optional - requires SSL certificates to be valid.
+class Config {
+
+    twitterConfig = {
+        consumer_key: process.env.CONSUMER_KEY,
+        consumer_secret: process.env.CONSUMER_SECRET,
+        access_token: process.env.ACCESS_TOKEN,
+        access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+        // timeout_ms: 60 * 1000,  // optional HTTP request timeout to apply to all requests.
+        // strictSSL: true,     // optional - requires SSL certificates to be valid.
+    }
+
+    cipher = this.encrypt(Date.now().toString())
+
+    checkCipherError(param) {
+        const decyptedCipher = this.decrypt(param)
+        if (Number(decyptedCipher) && Date.now() < Number(decyptedCipher) + 12 * 3600 * 1000) {
+            return false
+        }
+        return true
+    }
+
+    encrypt(param) {
+        return CryptoJS.AES.encrypt(param, process.env.KEY).toString()
+    }
+
+    decrypt(param) {
+        if (!param)
+            return ""
+        return CryptoJS.AES.decrypt(param, process.env.KEY).toString(CryptoJS.enc.Utf8)
+    }
+
 }
+
+module.exports = Config
--- index.js
+++ index.js
@@ -1,37 +1,200 @@
 const Twit = require('twit')
+const cors = require('cors')
 const config = require('./config')
+const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
+const express = require('express')
+const app = express()
+const Joi = require('@hapi/joi');
+const path = require('path');
+const port = process.env.PORT || 4200
+const Encryption = new config()
+const T = new Twit(Encryption.twitterConfig)
+const SUCCESS = "SUCCESS"
+const ERROR = "ERROR"
+const allowedOrigins = [
+    'https://tchitostwitterapi.herokuapp.com/', 'http://localhost:' + port
+];
 
-const T = new Twit(config)
+app.use(cors({
+    origin: function (origin, callback) {
+        // allow requests with no origin (like mobile apps or curl requests)
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    }
+}));
 
-function tweetIt(pTweetMsg) {
+//middle ware config
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+// View engine setup
+app.engine('handlebars', exphbs({
+    defaultLayout: '',
+}));
+app.set('view engine', 'handlebars');
 
-    T.post('statuses/update', { status: pTweetMsg }, tweeted)
+// Static folder
+app.use('/public', express.static(path.join(__dirname, 'public')));
+app.listen(port, () => console.log('server running on ' + port))
 
-    function tweeted(err, data, response) {
-        if (err) {
-            console.log('Smth went wrooooooooong', err)
-        } else {
-            console.log('nicely done')
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.post('/getTweets', (req, res) => {
+
+    if (Encryption.checkCipherError(req.body.cipher))
+        return res.status(401).send('cipher error')
+
+    const { error } = validateGetTweetData(req.body)
+    if (error) { return res.status(403).send(error.details[0].message) }
+
+    const params = {
+        q: req.body.search,
+        count: req.body.limit ? req.body.limit : 5,
+        lang: req.body.lang ? req.body.lang : 'en'
+    }
+
+    return getInfo(params).then(result => {
+        return res.end(JSON.stringify(result))
+    }).catch(err => {
+        console.log('getting tweets went wrong', err)
+        return res.status(500).send(JSON.stringify(err))
+    })
+
+});
+
+app.post('/postPlainTweet', (req, res) => {
+
+    if (Encryption.checkCipherError(req.body.cipher))
+        return res.status(401).send('cipher error')
+
+    const { error } = validatePostTweetData(req.body)
+    if (error) { return res.status(403).send(error.details[0].message) }
+
+    return tweetIt({ status: req.body.msgPost }).then(result => {
+        console.log('nicely done tweet sent')
+        return res.end(JSON.stringify(result))
+    }).catch(err => {
+        console.log('Smth went wrooooooooong in posting', err)
+        return res.status(500).send(JSON.stringify(err))
+    })
+
+});
+app.post('/postMediaTweet', async (req, res) => {
+
+    if (Encryption.checkCipherError(req.body.cipher))
+        return res.status(401).send('cipher error')
+
+    const { error } = validatePostMediaTweetData(req.body)
+    if (error) { return res.status(403).send(error.details[0].message) }
+
+    const medialistIds = []
+    for (const content of req.body.listB64Content) {
+        try {
+            const resp = await uploadMedia(content)
+            medialistIds.push(resp.data)
+        } catch (error) {
+            return res.status(500).send(error)
         }
 
     }
+
+    return tweetIt({ status: req.body.msgPost, media_ids: medialistIds }).then(result => {
+        console.log('Media tweet sent')
+        return res.end(JSON.stringify(result))
+    }).catch(err => {
+        console.log('Smth went wrooooooooong in posting media', err)
+        return res.status(500).send(JSON.stringify(err))
+    })
+
+});
+
+function uploadMedia(b64content) {
+
+    return new Promise((resolve, reject) => {
+
+        // first we must post the media to Twitter
+        T.post('media/upload', { media_data: b64content }, function (err, data, response) {
+            if (err)
+                reject(err)
+            else {
+                // now we can assign alt text to the media, for use by screen readers and
+                // other text-based presentations and interpreters
+                var mediaIdStr = data.media_id_string
+                var altText = "Small flowers in a planter on a sunny balcony, blossoming."
+                var meta_params = { media_id: mediaIdStr, alt_text: { text: altText } }
+
+                T.post('media/metadata/create', meta_params, function (err, data, response) {
+                    if (!err) {
+                        // now we can reference the media and post a tweet (media will attach to the tweet)
+                        resolve({ status: SUCCESS, data: mediaIdStr })
+                    } else {
+                        reject(err)
+                    }
+                })
+            }
+        })
+    })
+
 }
 
-function getTweets(pSearch) {
-    const params = {
-        q: pSearch,
-        count: 10,
-        // lang: 'fr'
-    }
+function tweetIt(param) {
 
-    T.get('search/tweets', params, gotData)
+    return new Promise((resolve, reject) => {
 
-    function gotData(err, data, response) {
-        console.log(data.statuses.map(res => res.text))
-    }
+        T.post('statuses/update', param, (err, data, response) => {
+            if (err) {
+                reject(err)
+            } else {
+                resolve(data)
+            }
+        })
+    })
 }
 
-//to tweet 
-tweetIt('hellow World')
+function getInfo(pSearchParams) {
+    return new Promise((resolve, reject) => {
 
-getTweets('javascript')
+        T.get('search/tweets', pSearchParams, (err, data, response) => {
+            if (err) {
+                reject(err)
+            } else {
+                resolve(data)
+            }
+        })
+    })
+}
+
+function validateGetTweetData(pData) {
+    const schema = Joi.object({
+        search: Joi.string().required(),
+        count: Joi.number(),
+        lang: Joi.string(),
+        cipher: Joi.string(),
+    })
+    return schema.validate(pData)
+}
+function validatePostTweetData(pData) {
+    const schema = Joi.object({
+        msgPost: Joi.string().required(),
+        cipher: Joi.string(),
+    })
+    return schema.validate(pData)
+}
+function validatePostMediaTweetData(pData) {
+    const schema = Joi.object({
+        msgPost: Joi.string().required(),
+        listB64Content: Joi.array().items(Joi.string().base64()).required(),
+        cipher: Joi.string(),
+    })
+    return schema.validate(pData)
+}
+
+console.log(Encryption.cipher)
