330750f8da2fdc9e3a4864838259040d7fc61fca
amine.tchitooss <amine.tchitooss@gmail.com>
2020-05-19 22:10:53 +0200

    style text generated

--- canvas.js
+++ canvas.js
@@ -8,6 +8,7 @@ class Draw {
         <!DOCTYPE html>
         <head>
             <meta charset="UTF-8">
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <meta http-equiv="X-UA-Compatible" content="ie=edge">
             <style>
